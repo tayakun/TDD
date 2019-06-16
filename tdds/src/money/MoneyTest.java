f312381093e336fe1c8def2cd6df8c83560077fd
tayatamn <tayakun@me.com>
2019-06-16 21:09:36 +0900

    第9章読了

--- tdds/src/money/MoneyTest.java
+++ tdds/src/money/MoneyTest.java
@@ -19,7 +19,7 @@ public class MoneyTest {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(new Franc(6)));
+		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 
@@ -28,5 +28,11 @@ public class MoneyTest {
 		Money five = Money.franc(5);
 		assertEquals(Money.franc(10), five.times(2));
 		assertEquals(Money.franc(15), five.times(3));
+	}
+
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
 	}
 }
