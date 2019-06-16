e112e56884d368a63d5ead3e2de815787574b317
tayatamn <tayakun@me.com>
2019-06-16 18:28:13 +0900

    第6章読了

--- tdds/src/money/Dollar.java
+++ tdds/src/money/Dollar.java
@@ -1,8 +1,6 @@
 package money;
 
-public class Dollar {
-
-	private int amount;
+public class Dollar extends Money{
 
 	public Dollar(int amount) {
 		this.amount = amount;
@@ -11,10 +9,5 @@ public class Dollar {
 
 	public Dollar times(int multiplier) {
 		return new Dollar(amount * multiplier);
-	}
-
-	public boolean equals(Object object) {
-		Dollar dollar = (Dollar) object;
-		return amount == dollar.amount;
 	}
 }
--- tdds/src/money/Franc.java
+++ tdds/src/money/Franc.java
@@ -1,19 +1,13 @@
 package money;
 
-public class Franc {
+public class Franc extends Money{
 
-	private int amount;
 	Franc (int amount) {
 		this.amount = amount;
 	}
 
 	Franc times(int multiplier) {
 		return new Franc(amount * multiplier);
-	}
-
-	public boolean equals(Object object) {
-		Franc franc = (Franc) object;
-		return amount == franc.amount;
 	}
 
 }
